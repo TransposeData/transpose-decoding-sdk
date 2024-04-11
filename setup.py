c73ba8de0904372b72f1005298f2fe7fcd8e258b
Patrick Croke <86120203+tannishmango@users.noreply.github.com>
2024-04-11 11:42:36 -0400

    update versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('README.md', 'r') as f: long_description = f.read()
     
 setup(
     name='transpose_decoding_sdk',
-    version='1.0.4',
+    version='1.0.5',
     
     # meta
     long_description=long_description,
